380cb94ef05e0b46d5465d1c3f2df4bcd055067f
AAYUSH116 <82816625+AAYUSH116@users.noreply.github.com>
2021-05-21 18:31:41 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
+noseX=0;
+noseY=0;
+difference=0;
+rightWristX=0;
+leftWristX=0;
+
 function setup(){
     video=createCapture(VIDEO);
     video.size(550,500);
-    canvas= createCanvas(550,550);
+    canvas= createCanvas(550,500);
     canvas.position(560,150);
     poseNet= ml5.poseNet(video,modelLoaded);
     poseNet.on('pose',gotPoses);
@@ -9,6 +15,11 @@ function setup(){
 
 function draw(){
     background('#ffcccc');
+
+    document.getElementById("square_side").innerHTML=difference+"px";
+    fill('#FF3232');
+    stroke('#ffff00 ');
+    square(noseX,noseY,difference);
 
 }
 
@@ -19,5 +30,12 @@ function modelLoaded(){
 function gotPoses(results){
     if(results.length>0){
         console.log(results);
+        noseX= results[0].pose.nose.x;
+        noseY=results[0].pose.nose.y;
+        console.log("noseX="+noseX+"noseY"+noseY);
+        leftWristX=results[0].pose.leftWrist.x;
+        rightWristX= results[0].pose.rightWrist.x;
+        difference=floor(leftWristX-rightWristX);
+        console.log("leftWristX="+leftWristX+"rightWristX"+rightWristX+"difference="+difference);
     }
 }
